543353159c3d744724a86815739cc1c23b699395
swetha <swetha.n2022cse@sece.ac.in>
2025-06-16 20:48:58 +0530

    first commit

--- db.js
+++ db.js
@@ -1,8 +1,32 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect('mongodb://127.0.0.1:27017/petcare');
+//     console.log("MongoDB connected");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/petcare');
+    const mongoURI = process.env.MONGODB_URI;
+    if (!mongoURI) {
+      throw new Error('MONGODB_URI environment variable not set');
+    }
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
